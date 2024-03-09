95f9b75ff58176736a998b5e5b226597435d7e27
Jiakuang He <jkuanghe@gmail.com>
2024-03-09 10:28:59 -0800

    Clean up code

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -7,15 +7,6 @@ import {update as updateAllCharactersData} from '../components/slices/allCharact
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDauC5xywKpwLy_WBOj6aaIVr0tAa0z4Rs",
-//   authDomain: "oa-virtoffice-project.firebaseapp.com",
-//   projectId: "oa-virtoffice-project",
-//   storageBucket: "oa-virtoffice-project.appspot.com",
-//   messagingSenderId: "416890512159",
-//   appId: "1:416890512159:web:59d7b08b2333270e6f062a",
-//   measurementId: "G-JDJCEPS4YH"
-// };
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
     authDomain: process.env.REACT_APP_authDomain,
@@ -24,6 +15,7 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_appId,
     measurementId: process.env.REACT_APP_measurementId,
+    // TODO: Remove to config file?
     databaseURL: "https://jiakuang-virtoffice-firebase-default-rtdb.firebaseio.com/",
   };
 
