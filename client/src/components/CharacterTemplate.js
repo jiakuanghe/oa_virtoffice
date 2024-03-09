e3681fc20aaec051cd5ad4dcef7b7c06077aee0b
Jiakuang He <jkuanghe@gmail.com>
2024-03-08 22:43:23 -0800

    Immigrate code to characterTemplate

---
+++ client/src/components/CharacterTemplate.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useContext} from 'react';
+import {connect} from 'react-redux';
+
+import CanvasConext from './CanvasContext';
+import {CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP} from './characterConstants';
+import {TILE_SIZE} from './mapConstants';
+import {loadCharacter} from './slices/statusSlice';
+import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
+
+function CharacterTemplate({ charactersData, loadCharacter }) {
+    const context = useContext(CanvasConext);
+    useEffect(() => {
+        if (context == null || charactersData == null) {
+            return;
+        }
+        const characterImg = document.querySelector(`#character-sprite-img-${MY_CHARACTER_INIT_CONFIG.characterClass}`);
+        const { sx, sy } = CHARACTER_CLASSES_MAP[MY_CHARACTER_INIT_CONFIG.characterClass].icon;
+        context.canvas.drawImage(
+            characterImg,
+            sx,
+            sy,
+            CHARACTER_IMAGE_SIZE - 5,
+            CHARACTER_IMAGE_SIZE - 5,
+            charactersData.position.x * TILE_SIZE,
+            charactersData.position.y * TILE_SIZE,
+            CHARACTER_IMAGE_SIZE,
+            CHARACTER_IMAGE_SIZE
+        );
+        loadCharacter(true);
+    }, [context, charactersData?.position.x, charactersData?.position.y, loadCharacter]);
+
+    return null;
+}
+
+export default connect(null, {loadCharacter})(CharacterTemplate);
--- client/src/components/MyCharacter.js
+++ client/src/components/MyCharacter.js
@@ -1,16 +1,13 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
 
-import CanvasConext from './CanvasContext';
-import {CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP} from './characterConstants';
-import {TILE_SIZE} from './mapConstants';
-import {loadCharacter} from './slices/statusSlice';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
 import {update as updateAllCharactersData} from './slices/allCharactersSlice'
 import {writeUserData} from '../firebase/firebase';
 
-function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData, webrtcSocket }) {
-    const context = useContext(CanvasConext);
+import CharacterTemplate from "./CharacterTemplate";
+
+function MyCharacter({ myCharactersData, updateAllCharactersData, webrtcSocket }) {
     useEffect(() => {
         const myInitData = {
             ...MY_CHARACTER_INIT_CONFIG,
@@ -26,33 +23,11 @@ function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData,
         updateAllCharactersData(users);
     }, [webrtcSocket]);
 
-    useEffect(() => {
-        if (context == null || myCharactersData == null) {
-            return;
-        }
-        const characterImg = document.querySelector(`#character-sprite-img-${MY_CHARACTER_INIT_CONFIG.characterClass}`);
-        const { sx, sy } = CHARACTER_CLASSES_MAP[MY_CHARACTER_INIT_CONFIG.characterClass].icon;
-        context.canvas.drawImage(
-            characterImg,
-            sx,
-            sy,
-            CHARACTER_IMAGE_SIZE - 5,
-            CHARACTER_IMAGE_SIZE - 5,
-            myCharactersData.position.x * TILE_SIZE,
-            myCharactersData.position.y * TILE_SIZE,
-            CHARACTER_IMAGE_SIZE,
-            CHARACTER_IMAGE_SIZE
-        );
-        loadCharacter(true);
-    }, [context, myCharactersData?.position.x, myCharactersData?.position.y, loadCharacter]);
-
-    return null;
+    return <CharacterTemplate charactersData={myCharactersData}/>
 }
 
 const mapStateToProps = (state) => {
     return {myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id]};
 };
 
-const mapDispatch = {loadCharacter, updateAllCharactersData};
-
-export default connect(mapStateToProps, mapDispatch)(MyCharacter);
+export default connect(mapStateToProps, {updateAllCharactersData})(MyCharacter);
